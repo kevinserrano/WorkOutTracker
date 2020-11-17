1c2b0a28fb9ca9658f136eae6521e0facefe2d50
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-11-17 13:05:18 -0600

    added api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,54 @@
+var db = require("../models");
+const {Workout} = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", (req,res) => {
+        db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        const workout = new db.Workout(req.body);
+        workout.updateTotalDuration();
+    
+        db.Workout.create(workout)
+        .then(results => {
+            res.json(results);
+        });
+    });
+
+    app.put("/api/workouts/:id", (req,res) => {
+        db.Workout.findOne({
+            _id: req.params.id
+        }).then (dbWorkout => {
+            let currentDuration = dbWorkout.totalDuration + req.body.duration;
+            db.Workout.upDateOne({
+                _id: req.params.id
+            }, {$push: {
+                exercises: req.body
+            }, totalDuration: currentDuration
+        }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err=> {
+            res.json(err);
+        })
+    })
+})
+
+    app.get("/api/workouts/range", (req,res) => {
+        db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+};
